f48e319fdcf0f18a5b55e356c9e99afa5ccd1582
kmakarov <kmakarov@kmakarov.TC.local>
2018-06-07 14:44:36 +0300

    321

---
+++ Object Repository/New Test Object (1).rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object (1)</name>
+   <tag></tag>
+   <elementGuidId>559ae7d9-3907-40d5-937c-e0ad0e4e50c5</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>e31231e0-a7f4-4a50-88f5-429483e591a4</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
